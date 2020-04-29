e1cfd416aa443e536b092683b3edf574481e1be2
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2020-04-29 22:26:30 +0300

    added ralign_to

--- inc/psl/algorithms.h
+++ inc/psl/algorithms.h
@@ -2,11 +2,35 @@
 
 namespace psl
 {
+	/**
+	 * \brief Aligns a value to the next possible value that satisfies the alignment
+	 *
+	 * \tparam T
+	 * \param[in] value
+	 * \param[in] alignment
+	 * \returns constexpr auto
+	 */
 	template <IsIntegral T>
 	constexpr inline auto align_to(T value, T alignment) noexcept
 	{
 		if(alignment <= T{1}) return value;
 		auto remainder = value % alignment;
 		return (remainder) ? value + (alignment - remainder) : value;
+	}
+
+	/**
+	 * \brief Aligns a value to the previous possible value that satisfies the alignment (including the seed value)
+	 *
+	 * \tparam T
+	 * \param[in] value
+	 * \param[in] alignment
+	 * \returns constexpr auto
+	 */
+	template <IsIntegral T>
+	constexpr inline auto ralign_to(T value, T alignment) noexcept
+	{
+		if(alignment <= T{1}) return value;
+		auto remainder = value % alignment;
+		return (remainder) ? value - remainder : value;
 	}
 } // namespace psl
