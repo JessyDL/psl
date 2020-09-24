ca344c7f97e2486d3a1155a88aceeb2fe7dc8056
Jessy De Lannoit <jessy.de.lannoit@gmail.com>
2020-09-24 23:51:54 +0300

    [tooling] github workflows

--- tests/source/tests.cpp
+++ tests/source/tests.cpp
@@ -1,1 +1,15 @@
-int main() { return 0; }
+#include <gtest/gtest.h>
+
+TEST(QuickTest, Add)
+{
+	ASSERT_EQ(2, 1 + 1);
+	ASSERT_EQ(5, 3 + 2);
+	ASSERT_EQ(10, 7 + 3);
+}
+
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	::testing::FLAGS_gtest_death_test_style = "fast";
+	return RUN_ALL_TESTS();
+}
